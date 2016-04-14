df79a544a0772a2ecf6be5445093007372776107
unknown <anders.langseth@MW7XP5FOKEGVR0.dir.svc.accenture.com>
2016-04-14 15:09:06 +0200

    yho

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularBluemixSeedApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularBluemixSeedApp
+ */
+angular.module('angularBluemixSeedApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
